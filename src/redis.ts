3808eb136ccc7bf7c127dd509508588b3a62bd8c
Ben Sandee <ben@orangebikelabs.com>
2025-05-12 10:29:16 -0500

    chore: migrate away from redis to valkey

--- src/redis.ts
+++ src/redis.ts
@@ -1,12 +1,49 @@
 import { configuration } from "./config";
 import { RedisStore } from "connect-redis";
-import { createClient, RedisClientType } from "redis";
+import Valkey from "iovalkey";
 import { logger, maskUriPassword } from "./utils";
+import { z } from "zod";
 
-let client: RedisClientType | undefined;
+let client: Valkey | undefined;
+
+type ValkeyConfiguration = {
+  db?: number;
+  host: string;
+  port: number;
+};
+
+const urlToValkeyConfiguration = (url: string): ValkeyConfiguration => {
+  const parsedUrl = new URL(url);
+  if (parsedUrl.protocol !== "valkey:" && parsedUrl.protocol !== "redis:") {
+    throw new Error(`Invalid valkey session URI: ${url}`);
+  }
+
+  const { success: successPort, data: port } = z.coerce
+    .number()
+    .min(1)
+    .max(65535)
+    .safeParse(parsedUrl.port);
+  if (!successPort) {
+    throw new Error(`Invalid port in valkey session URI: ${url}`);
+  }
+  const { success: successDatabaseId, data: db } = z.coerce
+    .number()
+    .nonnegative()
+    .optional()
+    .safeParse(parsedUrl.pathname.substring(1));
+  if (!successDatabaseId) {
+    throw new Error(`Invalid database ID in valkey session URI: ${url}`);
+  }
+  return {
+    db,
+    host: parsedUrl.hostname,
+    port,
+  };
+};
 
 if (isEnabled()) {
-  client = createClient({ url: configuration.redisUri });
+  const config = urlToValkeyConfiguration(configuration.redisUri);
+  client = new Valkey({ ...config, lazyConnect: true });
   client.on("error", (err) => {
     logger.warn(err, "Redis Client Error");
   });
@@ -27,9 +64,9 @@ async function start() {
   }
 }
 
-async function stop() {
+function stop() {
   if (client !== undefined) {
-    await client.disconnect();
+    client.disconnect();
     client = undefined;
   }
 }
